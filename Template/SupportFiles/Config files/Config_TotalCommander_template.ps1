f54cd8f20b57650b625b9456ebdc0acfd47026a2
roman prokhorov <i@rprokhorov.ru>
2019-12-31 11:27:10 +0300

    Add config for Total Commander

---
+++ Template/SupportFiles/Config files/Config_TotalCommander_template.ps1
@@ -0,0 +1,53 @@
+$Config = @{
+    appVendor = "%Publisher%"
+    appName = "%appName%"
+    appVersion = "%appVersion%"
+    appDetectionVersion = "%appDetectionVersion%"
+    appDetectionName = "%appDetectionName%"
+    appScriptDate = get-date -Format "yyyy/MM/dd"
+    appScriptAuthor = "RProkhorov"
+    close_app = "TOTALCMD", 'TOTALCMD64'
+    InstallScriptBlock = {
+        [string]$installPhase = 'Installation'
+		Show-InstallationProgress -StatusMessage "Установка приложения $appName `nШаг [1/4] установка приложения..." #-WindowLocation 'BottomRight'
+		# Установка MSI файла с параметрами
+        $mainExitCode = Execute-Process -Path "$dirFiles\%FileName%" -Parameters "/A1H1L4M1D1U1K1G1"
+        [string]$installPhase = 'Post-Installation'
+        
+        Show-InstallationProgress -StatusMessage "Установка приложения $appName `nШаг [2/4] Перемещение файлов..." #-WindowLocation 'BottomRight'
+        New-Item -ItemType Directory -Path 'C:\Program Files\Total Commander' -ErrorAction SilentlyContinue
+        Get-ChildItem -Path C:\totalcmd\* | Move-Item -Destination 'C:\Program Files\Total Commander' -ErrorAction SilentlyContinue
+        Remove-Item C:\totalcmd -Force -ErrorAction SilentlyContinue
+
+        Show-InstallationProgress -StatusMessage "Установка приложения $appName `nШаг [3/4] Создание ярлыка" #-WindowLocation 'BottomRight'
+        #New-Item "$env:SystemDrive\Users\Public\Desktop\VLC media player.lnk" -Force
+        $WshShell = New-Object -comObject WScript.Shell
+        $Shortcut = $WshShell.CreateShortcut("$env:SystemDrive\Users\Public\Desktop\Total Commander.lnk")
+        $Shortcut.TargetPath = "C:\Program Files\Total Commander\TOTALCMD.EXE"
+        $Shortcut.Save()
+        
+        Show-InstallationProgress -StatusMessage "Установка приложения $appName `nШаг [4/4] Создание ярлыка" #-WindowLocation 'BottomRight'
+        Set-ItemProperty -Path 'HKLM:\SOFTWARE\Ghisler\Total Commander\' -Name InstallDir -Value 'C:\Program Files\Total Commander'
+        if($envOSArchitecture -eq '64-bit')
+        {
+            Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Totalcmd64\' -Name UninstallString -Value 'C:\Program Files\Total Commander\tcunin64.exe'
+            Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Totalcmd64\' -Name DisplayIcon -Value 'C:\Program Files\Total Commander\tcunin64.exe'
+        }
+        else 
+        {
+            Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Totalcmd\' -Name UninstallString -Value 'C:\Program Files\Total Commander\tcunin.exe'
+            Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Totalcmd\' -Name DisplayIcon -Value 'C:\Program Files\Total Commander\tcunin.exe'
+        }
+    }
+    UninstallScriptBlock = {
+        [string]$installPhase = 'Uninstall'
+        if($envOSArchitecture -eq '64-bit')
+        {
+            $mainExitCode = Execute-Process -Path "C:\Program Files\Total Commander\tcunin64.exe" -Parameters "/7"
+        }
+        else
+        {
+            $mainExitCode = Execute-Process -Path "C:\Program Files\Total Commander\tcunin.exe" -Parameters "/7"
+        }
+    }
+}
