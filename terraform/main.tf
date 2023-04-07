b7d9e4a3d9ae01190940831a5507e423db8564f3
Roman Prokhorov <i@rprokhorov.ru>
2023-04-07 11:03:13 +0300

    Moved more params to variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,16 +38,17 @@ locals {
 }
 
 resource "azurerm_resource_group" "resource_group" {
-  name     = "${var.azurerm_resource_group.name}-rg"
+  name     = "${var.azurerm_resource_group.name}"
   location = var.azurerm_resource_group.location
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.azurerm_storage_account.name}storage"
+  name                     = "${var.azurerm_storage_account.name}"
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  account_kind             = "StorageV2"
 }
 
 resource "azurerm_storage_container" "storage_container" {
@@ -67,7 +68,7 @@ resource "azurerm_storage_blob" "tamopsblobs" {
 }
 
 resource "azurerm_app_service_plan" "app_service_plan" {
-  name                = "${var.azurerm_app_service_plan.name}-app-service-plan"
+  name                = "${var.azurerm_app_service_plan.name}"
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
   kind                = "FunctionApp"
@@ -92,11 +93,9 @@ resource "azurerm_linux_function_app" "function_app" {
       powershell_core_version = "7.2"
     }
   }
-
 }
 
-
-resource "azurerm_function_app_function" "example" {
+resource "azurerm_function_app_function" "httpTrigger" {
   name            = "application"
   function_app_id = azurerm_linux_function_app.function_app.id
   language        = "PowerShell"
@@ -106,8 +105,17 @@ resource "azurerm_function_app_function" "example" {
     content = file("${path.module}/../azure_functions/http_trigger_run.ps1")
   }
 
+  file {
+    name    = "../Modules/software-check/software-check.psd1"
+    content = file("${path.module}/../Modules/software-check/software-check.psd1")
+  }
+  file {
+    name    = "../Modules/software-check/software-check.psm1"
+    content = file("${path.module}/../Modules/software-check/software-check.psm1")
+  }
+
   test_data = jsonencode({
-    "name" = "Azure"
+    "name" = "git"
   })
 
   config_json = jsonencode({
